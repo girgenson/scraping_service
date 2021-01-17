1cf07918971d3329cd4c3d5786fcf0a4398b4ff9
girgenson <vadimgirgenson@yandex.ru>
2021-01-17 20:59:35 +0300

    Custom user senntings accomplished, greated dump of migrations in .json file

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,39 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, \
+    BaseUserManager
 from django.contrib.auth.models import UserManager
 from django.db import models
 from django.db.models import ForeignKey
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
 class MyUser(AbstractBaseUser):
@@ -13,12 +45,12 @@ class MyUser(AbstractBaseUser):
     date_of_birth = models.DateField()
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
-    city = ForeignKey('City', on_delete=models.SET_NULL,
+    city = ForeignKey('scraping.City', on_delete=models.SET_NULL,
                       null=True, blank=True)
-    language = ForeignKey('Language', on_delete=models.SET_NULL,
+    language = ForeignKey('scraping.Language', on_delete=models.SET_NULL,
                       null=True, blank=True)
     send_email = models.BooleanField(default=True)
-    objects = UserManager()
+    objects = MyUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
