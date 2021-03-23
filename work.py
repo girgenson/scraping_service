e2790024093cb9d9adb38ce5a94b8797240f5f90
girgenson <vadimgirgenson@yandex.ru>
2021-03-23 23:53:10 +0300

    Third parser is done

--- work.py
+++ work.py
@@ -1,7 +1,9 @@
 import requests
 import codecs
 from bs4 import BeautifulSoup as BS
+import mysqlx
 
+a = mysqlx.connection
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
@@ -46,34 +48,70 @@ def rabota(url):
     resp = requests.get(url, headers=headers)
     if resp.status_code == 200:
         soup = BS(resp.content, 'html.parser')
-        table = soup.find('table', id='ct100_content_vacancyList_gridList')
-        if table:
-            tr_list = table.find_all('tr', attrs={'id': True})
-            for tr in tr_list:
-                div = tr.find('div', attrs={'class': 'card-body'})
-                if div:
-                    title = div.find('p', attrs={'class': 'card-title'})
-                    href = title.a['href']
-                    content = tr.p.text
-                    company = 'No name'
-                    p = tr.find('p', attrs={'class': 'card-title'})
-                    if p:
-                        company = p.a.text
-                    jobs.append({'title': title.text,
-                                 'url': domain + href,
-                                 'description': content,
-                                 'company': company,
-                                 })
+        new_jobs = soup.find('div', attrs={'class': 'f-vacancylist-newnotfound'})
+        if not new_jobs:
+            table = soup.find('table', id='ct100_content_vacancyList_gridList')
+            if table:
+                tr_list = table.find_all('tr', attrs={'id': True})
+                for tr in tr_list:
+                    div = tr.find('div', attrs={'class': 'card-body'})
+                    if div:
+                        title = div.find('p', attrs={'class': 'card-title'})
+                        href = title.a['href']
+                        content = tr.p.text
+                        company = 'No name'
+                        p = tr.find('p', attrs={'class': 'card-title'})
+                        if p:
+                            company = p.a.text
+                        jobs.append({'title': title.text,
+                                     'url': domain + href,
+                                     'description': content,
+                                     'company': company,
+                                     })
+            else:
+                errors.append({'url': url, 'title': "Table doesn't exist"})
         else:
-            errors.append({'url': url, 'title': "Table doesn't exist"})
+            errors.append({'url': url, 'title': "Page is empty"
+                                                ""})
+    else:
+        errors.append({'url': url, 'title': "Page doesn't response"})
+    return jobs, errors
+
+
+def dou(url):
+    jobs = []
+    errors = []
+    # domain = 'https://www.work.ua'
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        soup = BS(resp.content, 'html.parser')
+        main_div = soup.find('div', id='vacancyListId')
+        if main_div:
+            li_lst = main_div.find_all('li', attrs={'class': 'job-link'})
+            for li in li_lst:
+                title = li.find('div', attrs={'class': 'title'})
+                href = title.a['href']
+                cont = li.find('div', attrs={'class': 'sh-info'})
+                content = cont.text
+                company = 'No name'
+                a = title.find('a', attrs={'class': 'company'})
+                if a:
+                    company = a.text
+                jobs.append({'title': title.text,
+                             'url': href,
+                             'description': content,
+                             'company': company,
+                             })
+        else:
+            errors.append({'url': url, 'title': "Div doesn't exist"})
     else:
         errors.append({'url': url, 'title': "Page doesn't response"})
     return jobs, errors
 
 
 if __name__ == '__main__':
-    url = 'https://rabota.ua/zapros/python/'
-    jobs, errors = rabota(url)
+    url = 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D0%B5%D0%B2&category=Python'
+    jobs, errors = dou(url)
     h = codecs.open('work.txt', 'w', 'utf-8')
     h.write(str(jobs))
     h.close()
