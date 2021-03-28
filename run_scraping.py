6f6267ba58548b653fa6d256f63adfaa510ad363
girgenson <vadimgirgenson@yandex.ru>
2021-03-28 23:52:16 +0300

    Vacancies 2 database

--- run_scraping.py
+++ run_scraping.py
@@ -2,6 +2,8 @@ import codecs
 import os
 import sys
 
+from django.db import DatabaseError
+
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
 os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
@@ -19,13 +21,20 @@ parsers = ((work, 'https://www.work.ua/ru/jobs-kyiv-python/'),
            (djinni, 'https://djinni.co/jobs/?keywords=python+%D0%BA%D0%B8%D0%B5%D0%B2')
 )
 
-city = City.objects.filter(slug='kiev')
+city = City.objects.filter(slug='kiev').first()
+language = Language.objects.filter(slug='python').first()
 jobs, errors = [], []
 for func, url in parsers:
     j, e = func(url)
     jobs += j
     errors += e
 
-h = codecs.open('parsers.txt', 'w', 'utf-8')
-h.write(str(jobs))
-h.close()
+for job in jobs:
+    v = Vacancy(**job, city=city, language=language)
+    try:
+        v.save()
+    except DatabaseError:
+        pass
+
+
+
