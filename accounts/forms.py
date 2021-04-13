5b78101b1d8c6e38929722eaefbf15f45469d41e
spesh <ackount6shots@gmail.com>
2021-04-13 16:48:59 +0300

    Registration and login forms, views and pages

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.hashers import check_password
 
 User = get_user_model()
 
+
 class UserLoginForm(forms.Form):
     email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
@@ -24,3 +25,19 @@ class UserLoginForm(forms.Form):
                 raise forms.ValidationError('Данный аккаунт отключен')
 
         return super(UserLoginForm, self).clean(*args, **kwargs)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    email = forms.CharField(label='Введите email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Введите пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Введите пароль еще раз', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('email', )
+
+    def clean_password2(self):
+        data = self.cleaned_data
+        if data['password'] != data['password2']:
+            raise forms.ValidationError('Пароли не совпадают')
+        return data['password2']
