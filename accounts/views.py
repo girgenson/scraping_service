5a11517dcefb65ddd94a0e7e726b2f495791b6d1
girgenson <vadimgirgenson@yandex.ru>
2021-04-14 00:15:23 +0300

    User information popups

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, get_user_model
-
+from django.contrib import messages
 from accounts.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
 
 
@@ -30,6 +30,7 @@ def register_view(request):
         new_user = form.save(commit=False)
         new_user.set_password(form.cleaned_data['password'])
         new_user.save()
+        messages.success(request, 'Пользователь добавлен в систему.')
         return render(request, 'accounts/register_done.html', {'new_user': new_user})
     return render(request, 'accounts/register.html', {'form': form})
 
@@ -45,6 +46,7 @@ def update_view(request):
                 user.language = data['language']
                 user.send_email = data['send_email']
                 user.save()
+                messages.success(request, 'Данные сохранены.')
                 return redirect('accounts:update')
 
         form = UserUpdateForm(
@@ -61,4 +63,5 @@ def delete_view(request):
         if request.method == 'POST':
             qs = User.objects.get(pk=user.pk)
             qs.delete()
+            messages.error(request, 'Пользователь удален.')
     return redirect('home')
