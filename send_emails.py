58a36d7b4f56e63fc191e1b6a96266b62fe64228
girgenson <vadimgirgenson@yandex.ru>
2021-04-16 19:27:32 +0300

    Send email function in progress

--- send_emails.py
+++ send_emails.py
@@ -13,31 +13,40 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
 
 django.setup()
 from scraping.models import Vacancy
+from scraping_service.settings import EMAIL_HOST_USER
+subject = "Рассылка вакансий"
+text_content = "Рассылка вакансий"
+from_email = EMAIL_HOST_USER
 
-
-
+empty = '<h2>К сожалению, на сегодня по вашему запросу нет вакансий</h2>'
 User = get_user_model()
 qs = User.objects.filter(send_email=True).values('city', 'language', 'email')
-users_dict = {}
+users_dct = {}
 for i in qs:
-    users_dict.setdefault((i['city'], i['language']), [])
-    users_dict[(i['city'], i['language'])].append(i['email'])
-if users_dict:
-    params = {'city_id_in': [], 'language_id__in': []}
-    for pair in users_dict.keys():
-        params['city_id_in'].append(pair[0])
-        params['city_id_in'].append(pair[1])
-    qs = Vacancy.objects.filter(**params).values[:10]
+    users_dct.setdefault((i['city'], i['language']), [])
+    users_dct[(i['city'], i['language'])].append(i['email'])
+if users_dct:
+    params = {'city_id__in': [], 'language_id__in': []}
+    for pair in users_dct.keys():
+        params['city_id__in'].append(pair[0])
+        params['language_id__in'].append(pair[1])
+    qs = Vacancy.objects.filter(**params).values()
     vacancies = {}
     for i in qs:
-        vacancies.setdefault((i['city_id'], i['language_id']))
+        vacancies.setdefault((i['city_id'], i['language_id']), [])
         vacancies[(i['city_id'], i['language_id'])].append(i)
+    if vacancies:
+        for keys, emails in users_dct.items():
+            rows = vacancies.get(keys, [])
+            html = ''
+            for row in rows:
+                html += f'<h5><a href="{ row["url"] }">{ row["title"] }</a></h5>'
+                html += f'<p>{row["description"]}</p>'
+                html += f'<p>{row["company"]}</p><br><hr>'
+            _html = html if html else empty
+            for email in emails:
+                to = email
+                msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+                msg.attach_alternative(_html, "text/html")
+                msg.send()
 
-
-
-subject, from_email, to = 'hello', 'from@example.com', 'to@example.com'
-text_content = 'This is an important message.'
-html_content = '<p>This is an <strong>important</strong> message.</p>'
-msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-msg.attach_alternative(html_content, "text/html")
-msg.send()
